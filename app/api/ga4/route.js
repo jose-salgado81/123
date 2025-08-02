bf27b486f745b709420614de33ee57fc8fbfe2c8
Jose Manuel <hola@josemanuelsalgado.com>
2025-08-02 07:31:32 -0700

    ga4 1

--- app/api/ga4/route.js
+++ app/api/ga4/route.js
@@ -1,28 +1,10 @@
-import { NextResponse } from 'next/server';
-
+// app/api/extractvars/route.js
 export async function POST(request) {
-  try {
-    const body = await request.json();
-    
-    // Extracting the necessary data from the request body
-    const eventName = body.event_name;
-    const fbpValue = body.user_data?.fbp;
-    const gaCookieValue = body.user_data?.ga_cookie;
-    
-    // Log the received data
-    console.log('Received event:', eventName);
-    console.log('FBP:', fbpValue);
-    console.log('GA Cookie:', gaCookieValue);
+  const body = await request.json();
+  console.log("Received data:", body);
 
-    // You can add your server-side logic here. For example, 
-    // sending this data to another service or database.
-
-    // Return a successful response with a 204 status code (No Content)
-    return new NextResponse(null, { status: 204 });
-
-  } catch (error) {
-    console.error('Error processing request:', error);
-    // Return an error response
-    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
-  }
+  return new Response(JSON.stringify({ message: 'Data received very well', data: body }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
