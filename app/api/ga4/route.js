87769443d8fac1a3aeb672a435d3374e8ff4b203
Jose Manuel <hola@josemanuelsalgado.com>
2025-08-02 07:57:50 -0700

    sd

--- app/api/ga4/route.js
+++ app/api/ga4/route.js
@@ -1,10 +1,15 @@
-// app/api/extractvars/route.js
 export async function POST(request) {
+  console.log("Request method:", request.method); // Add this line
   const body = await request.json();
   console.log("Received data:", body);
 
   return new Response(JSON.stringify({ message: 'Data received very well', data: body }), {
     status: 200,
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
   });
 }
