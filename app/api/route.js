59e83d1c4d3720111bb485eb5f43f1922d10877f
Jose Manuel <hola@josemanuelsalgado.com>
2025-08-02 07:25:59 -0700

    very well

--- app/api/route.js
+++ app/api/route.js
@@ -3,7 +3,7 @@ export async function POST(request) {
   const body = await request.json();
   console.log("Received data:", body);
 
-  return new Response(JSON.stringify({ message: 'Data received', data: body }), {
+  return new Response(JSON.stringify({ message: 'Data received very well', data: body }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
