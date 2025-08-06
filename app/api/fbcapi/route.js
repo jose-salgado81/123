8035c824228a416bd6471d55c7f16bb5de42c931
Jose Manuel <hola@josemanuelsalgado.com>
2025-08-06 11:41:05 -0700

    fbcapi update

--- app/api/fbcapi/route.js
+++ app/api/fbcapi/route.js
@@ -1,44 +1,53 @@
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type',
-};
+const accessToken = 'EAAUmJiUQKVgBPFywdSncYzj9FTdWIFbkQ4wYpmi0vN3y8jSdq3ZBplEF6qgGoW04psTm8FEg7lqQNLwBm2NLB4PbljJ7ZB0B6J6EwtQd9ow7b9bbShABZCCJZCysx8PZCBOzFHBURsQw9KBcPsrfZCe1omnYv5lvPCyQZCTXqaZA9LRjZCHhIJUIbOmBz69yD3nZCRCiCnMYg8QZCKkMp1s1G3ZCjreUci16DML3fwAf';
+const pixelId = '1051240707176632';
 
-export async function POST(request) {
-  try {
-    const body = await request.json();
-    console.log("Received data:", body);
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { ga4, fbp, price, currency, event_name, product_name } = req.body;
 
-    return new Response(
-      JSON.stringify({ message: 'Data received very well', data: body }),
-      {
-        status: 200,
-        headers: {
-          ...corsHeaders,
-          'Content-Type': 'application/json',
+    const fbcapiPayload = {
+      data: [{
+        event_name: event_name,
+        event_time: Math.floor(Date.now() / 1000), 
+        user_data: {
+          fbp: fbp,
         },
-      }
-    );
-  } catch (error) {
-    return new Response(
-      JSON.stringify({ error: 'Invalid JSON or server error' }),
-      {
-        status: 400,
+        custom_data: {
+          currency: currency,
+          value: price, 
+          content_type: 'product',
+          content_name: product_name,
+          content_ids: [product_name]
+        }
+      }]
+    };
+
+    const fbcapiEndpoint = `https://graph.facebook.com/v19.0/${pixelId}/events?access_token=${accessToken}`;
+
+    try {
+      const response = await fetch(fbcapiEndpoint, {
+        method: 'POST',
         headers: {
-          ...corsHeaders,
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
+        body: JSON.stringify(fbcapiPayload)
+      });
+
+      const data = await response.json();
+      console.log('FBCAPI response:', data);
+
+      if (response.ok) {
+        res.status(200).json({ status: 'success', message: 'Event sent to Facebook CAPI' });
+      } else {
+        console.error('FBCAPI Error:', data);
+        res.status(response.status).json({ status: 'error', message: 'Failed to send event to Facebook CAPI', details: data });
       }
-    );
+
+    } catch (error) {
+      console.error('Fetch error:', error);
+      res.status(500).json({ status: 'error', message: 'Server error during fetch' });
+    }
+  } else {
+    res.status(405).json({ status: 'error', message: 'Method Not Allowed' });
   }
-}
-
-export async function OPTIONS() {
-  return new Response(null, {
-    status: 204,
-    headers: {
-      ...corsHeaders,
-      'Content-Length': '0',
-    },
-  });
 }
