f917612f996442c604e0b6e0d7183c7c71cc9b8c
Jose Manuel <hola@josemanuelsalgado.com>
2025-08-19 07:38:37 -0700

    sdf

--- app/api/route.js
+++ app/api/route.js
@@ -1,3 +1,15 @@
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
+
+
 export async function POST(request) {
   const body = await request.json();
   console.log("Received data:", body);
